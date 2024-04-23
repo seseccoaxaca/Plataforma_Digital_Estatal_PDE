988d0d41832296f2a4125e3851225d39a7400acc
SESECC OAXACA <100152504+seseccoaxaca@users.noreply.github.com>
2024-04-23 09:12:50 -0600

    Add files via upload

---
+++ layout/menu2.php
@@ -0,0 +1,136 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="keywords" content="" />
+	<meta name="author" content="" />
+	<meta name="robots" content="" />
+	<meta name="description" content="" />
+	<meta property="og:title" content="PDE - Oaxaca"/>
+	<meta property="og:description" content="" />
+	<meta property="og:image" content="" />
+	<meta name="format-detection" content="telephone=no">
+	
+	<!-- FAVICONS ICON -->
+	<link rel="icon" href="images/sistemas2/Logotipos/d_16.png" type="images/sistemas2/Logotipos/d_16.png" />
+	<link rel="shortcut icon" type="images/sistemas2/Logotipos/d_16.png" href="images/sistemas2/Logotipos/d_16.png" />
+	
+	<!-- PAGE TITLE HERE -->
+	<title>Plataforma Digital Estatal</title>
+	
+	<!-- MOBILE SPECIFIC -->
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	
+	<!--[if lt IE 9]>
+	<script src="js/html5shiv.min.js"></script>
+	<script src="js/respond.min.js"></script>
+	<![endif]-->
+	
+	<!-- STYLESHEETS -->
+	
+	<link rel="stylesheet" type="text/css" href="css/plugins.css">
+	<link rel="stylesheet" type="text/css" href="css/style.min.css">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+
+	<link class="skin" rel="stylesheet" type="text/css" href="css/skin/skin-1.css">
+	<link rel="stylesheet" type="text/css" href="css/templete.min.css">
+	<!-- Revolution Slider Css -->
+	<link rel="stylesheet" type="text/css" href="plugins/revolution/revolution/css/settings.css">
+	<link rel="stylesheet" type="text/css" href="plugins/revolution/revolution/css/navigation.css">
+	<!-- Revolution Navigation Style -->
+	
+	
+</head>
+<body id="bg">
+<div class="page-wraper">
+    <!-- header -->
+	<header class="site-header header-style-6">
+        <!-- top bar -->
+        <div class="top-bar">
+            <div class="container">
+                <div class="row d-flex justify-content-between">
+					<div class="col-lg-9 col-md-12 offset-lg-3 offset-md-0 top-bar-curve">
+						<div class="dez-topbar-left list-unstyled e-p-bx "> 
+							<ul>
+								<li><i class="fa fa-envelope"></i>sesecc@oaxaca.gob.mx</li>
+								<li><i class="fa fa-phone"></i>+52 951 206 69 11</li>
+							</ul>	
+						</div>
+						<div class="dez-topbar-right">
+							<ul class="social-bx list-inline float-right">
+								<li><a href="https://www.facebook.com/SESECCOaxaca/" class="fa fa-facebook"></a></li>
+								<li><a href="https://twitter.com/SESECCOaxaca" class="fa fa-twitter"></a></li>
+							</ul>
+						</div>
+						
+					</div>	
+                </div>
+            </div>
+        </div>
+        <!-- top bar END-->
+        <!-- main header -->
+        <div class="sticky-header main-bar-wraper navbar-expand-lg">
+            <div class="main-bar clearfix ">
+                <div class="container clearfix">
+                    <!-- website logo -->
+                    <div class="logo-header mostion">
+						<a href="index.php"><img src="images/sistemas2/Logotipos/Logo_360x78_v2.png"  alt=""></a>
+					</div>
+					
+                    <!-- nav toggle button -->
+					<button class="navbar-toggler collapsed navicon justify-content-end" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+						<span></span>
+						<span></span>
+						<span></span>
+					</button>
+						
+                    <!-- extra nav 
+					<div class="extra-nav">
+						<div class="extra-cell">
+							<button type="button" class="site-button"><i class="fa fa-search"></i>
+							<p><a href="sistema_s1.html">Servidores Públicos</a></p>
+							</button>
+						</div>
+					</div>
+					-->
+					
+
+					<!-- extra nav -->
+					<div class="extra-nav">
+						<div class="extra-cell">
+							<a href="sistema_s1.php"><button type="button" class="site-button"><i class="fa fa-search"></i></button></a>
+						</div>
+					</div>
+					
+					<!-- Quik search -->
+					<div class="dez-quik-search bg-primary bg-primary-dark">
+						<form action="#">
+							<input name="search" value="Busqueda anticorrupción" type="text" class="form-control" placeholder="Type to search">
+							<span  id="quik-search-remove"><i class="fa fa-remove"></i></span>
+						</form>
+					</div>
+					
+                    <!-- main nav -->
+                    <div class="header-nav navbar-collapse collapse justify-content-end" id="navbarNavDropdown">
+                        <ul class=" nav navbar-nav">
+							<!--<li class="active"> --> <li><a href="index.php">Inicio<i class="fa fa-chevron-down"></i></a>
+							</li>
+							<li class="active"><a href="despliegues.php">Entes Interconectados<i class="fa fa-chevron-down"></i></a>
+							</li>
+							<li > <a href="enproceso.php">Avisos de Privacidad<i class="fa fa-chevron-down"></i></a>
+							</li>
+							<li> <a href="enproceso.php">Declaratorias<i class="fa fa-chevron-down"></i></a>						
+							</li>
+							<li > <a href="retos.php">Retos<i class="fa fa-chevron-down"></i></a>				
+							</li>
+							<li>
+								<a href="sesion.html">Inicar Sesión<i class="fa fa-chevron-down"></i></a>
+							</li>
+						</ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- main header END -->
+    </header>
---
+++ layout/piepagina.php
@@ -0,0 +1,121 @@
+ <!-- Content END-->
+	<div class="chat-button" id="chatButton">
+        <img src="images/sistemas2/asistente/chat_verde.png" alt="Chat Icon" title="Asistente Virtual">
+    </div>
+    <div class="chat-popup" id="chatPopup">
+        <div class="chat-content">
+            <!-- Aquí coloca el contenido del chatbot -->
+			<div class="chat-container">
+				<div class="chat-header">
+					<h2 style="color:#9e264b !important;"><strong>PDE-Oaxaca</strong></h2><hr>
+					<p>Bienvenido al Asistente virtual, ¿En que podemos ayudarte?</p>
+					<button hidden id="toggleChat">Abrir Chat</button>
+				</div>
+				<div class="chat-body" id="chatBody">
+					<!-- Aquí se mostrarán los mensajes del chat -->
+				</div>
+				<div class="chat-footer">
+					<div class="row">
+						<div class="col-md-8">
+							<input type="text" id="userInput" class="form-control" placeholder="Escribe un mensaje...">
+						</div>
+						<div class="col-md-4">
+							<button id="sendMessage" class="btn btn-primary" >Enviar</button>
+						</div>
+					</div>
+				</div>
+			</div>
+        </div>
+        <span class="close" id="closeChat">&times;</span>
+    </div>
+    <!-- Footer -->
+	<footer class="site-footer">
+		<div class="footer-top">
+			<div class="container">
+				<div class="row">
+					<div class="col-lg-3 col-md-6 col-sm-6 footer-col-4">
+						<div class="widget widget_about" align="center">
+							<div class="logo-footer"><img src="images/SESECC_blanco.png" alt=""></div>
+							<p class="m-tb20">Visita nuestras redes sociales</p>
+							<ul class="dez-social-icon dez-social-icon-lg">
+								<li><a href="https://www.facebook.com/SESECCOaxaca/" class="fa fa-facebook fb-btn"></a></li>
+								<li><a href="https://twitter.com/SESECCOaxaca" class="fa fa-twitter tw-btn"></a></li>
+							</ul>
+						</div>
+					</div>
+					<div class="col-lg-3 col-md-6 col-sm-6 footer-col-4">
+					</div>
+					<div class="col-lg-3 col-md-6 col-sm-6 footer-col-4">
+						<div class="widget widget_services">
+							<h4 class="m-b15 text-uppercase">Dependencias</h4>
+							<div class="dez-separator bg-primary"></div>
+							<ul>
+								<li><a href="https://www.osfeoaxaca.gob.mx">Auditoría Superior de Fiscalización del Estado de Oaxaca</a></li>
+                                <li><a href="https://fge.oaxaca.gob.mx/index.php/fiscalia-combate-a-la-corrupcion">Fiscalía Especializada en Materia de Combate a la Corrupción</a></li>
+                                <li><a href="https://www.oaxaca.gob.mx/sctg/">Secretaría de Honestidad, Transparencia y Función Pública</a></li>
+                                <li><a href="https://www.tribunaloaxaca.gob.mx">Poder Judicial del Estado de Oaxaca</a></li>
+                                <li><a href="https://ogaipoaxaca.org.mx/site/">Órgano Garante de Acceso a la Información Pública, Transparencia y Protección de Datos Personales y Buen Gobierno del Estado de Oaxaca</a></li>
+                                <li><a href="https://tjaoaxaca.gob.mx">Tribunal de Justicia Administrativa y Combate a la Corrupción del Estado de Oaxaca </a></li>
+							</ul>
+						</div>
+					</div>
+					<div class="col-lg-3 col-md-6 col-sm-6 footer-col-4">
+						<div class="widget widget_getintuch">
+							<h4 class="m-b15 text-uppercase">Contacto</h4>
+							<div class="dez-separator bg-primary"></div>
+							<ul>
+								<li><i class="fa fa-map-marker"></i><strong>Dirección</strong> Colón No. 1128, Colonia Centro, Oaxaca de Juárez, Oaxaca </li>
+								<li><i class="fa fa-phone"></i><strong>Teléfono</strong>+951 206 69 11</li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</footer>
+    <!-- Footer END-->
+    <!-- scroll top button -->
+    <button class="scroltop style3 radius fa fa-arrow-up" ></button>
+</div>
+<!-- JavaScript  files ========================================= -->
+<script src="script/script.js"></script>
+<script src="js/jquery.min.js"></script><!-- JQUERY.MIN JS -->
+<script src="plugins/bootstrap/js/popper.min.js"></script><!-- BOOTSTRAP.MIN JS -->
+<script src="plugins/bootstrap/js/bootstrap.min.js"></script><!-- BOOTSTRAP.MIN JS -->
+<script src="plugins/bootstrap-select/bootstrap-select.min.js"></script><!-- FORM JS -->
+<script src="plugins/bootstrap-touchspin/jquery.bootstrap-touchspin.js"></script><!-- FORM JS -->
+<script src="plugins/magnific-popup/magnific-popup.js"></script><!-- MAGNIFIC POPUP JS -->
+<script src="plugins/counter/waypoints-min.js"></script><!-- WAYPOINTS JS -->
+<script src="plugins/counter/counterup.min.js"></script><!-- COUNTERUP JS -->
+<script src="plugins/imagesloaded/imagesloaded.js"></script><!-- IMAGESLOADED -->
+<script src="plugins/masonry/masonry-3.1.4.js"></script><!-- MASONRY -->
+<script src="plugins/masonry/masonry.filter.js"></script><!-- MASONRY -->
+<script src="plugins/owl-carousel/owl.carousel.js"></script><!-- OWL SLIDER -->
+<script src="plugins/lightgallery/js/lightgallery-all.js"></script><!-- LIGHT GALLERY -->
+<script src="js/custom.min.js"></script><!-- CUSTOM FUCTIONS  -->
+<script src="js/dz.carousel.min.js"></script><!-- SORTCODE FUCTIONS  -->
+
+<script src="js/dz.ajax.js"></script><!-- CONTACT JS  -->
+<!-- REVOLUTION JS FILES -->
+<script src="plugins/revolution/revolution/js/jquery.themepunch.tools.min.js"></script>
+<script src="plugins/revolution/revolution/js/jquery.themepunch.revolution.min.js"></script>
+<!-- Slider revolution 5.0 Extensions  (Load Extensions only on Local File Systems !  The following part can be removed on Server for On Demand Loading) -->
+<script src="plugins/revolution/revolution/js/extensions/revolution.extension.actions.min.js"></script>
+<script src="plugins/revolution/revolution/js/extensions/revolution.extension.carousel.min.js"></script>
+<script src="plugins/revolution/revolution/js/extensions/revolution.extension.kenburn.min.js"></script>
+<script src="plugins/revolution/revolution/js/extensions/revolution.extension.layeranimation.min.js"></script>
+<script src="plugins/revolution/revolution/js/extensions/revolution.extension.navigation.min.js"></script>
+<script src="plugins/revolution/revolution/js/extensions/revolution.extension.parallax.min.js"></script>
+<script src="plugins/revolution/revolution/js/extensions/revolution.extension.slideanims.min.js"></script>
+<script src="plugins/revolution/revolution/js/extensions/revolution.extension.video.min.js"></script>
+<script src="js/rev.slider.js"></script>
+<script>
+jQuery(document).ready(function() {
+	'use strict';
+	dz_rev_slider_2();
+});	/*ready*/
+</script>
+<div id="loading-area"></div>
+
+</body>
+</html>
